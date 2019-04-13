137f7b445bafdf3cfd635d4f39c6c6760b8f4295
Igor Moiseyenko <imoiseyenko93@gmail.com>
2019-04-13 12:28:41 +0200

    Const function adjustment.

--- lab1/costFunction.m
+++ lab1/costFunction.m
@@ -5,10 +5,10 @@ function [J, grad] = costFunction(theta, X, y)
   J = 0;
   grad = zeros(size(theta));
 
-  h = sigmoid(X * theta); % size(X) = [100 3]; size(theta) = [3 1];
+  h = sigmoid(X * theta);
   term1 = (-y) .* log(h);
   term2 = (1 - y) .* log(1 - h);
-  J = sum(term1 - term2) / m;
+  J = sum(sum(term1 - term2)) / m;
 
   grad = (1 / m) * (X' * (h - y));
 
